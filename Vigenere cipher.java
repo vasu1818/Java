acc073c1384a0203ded732796947a1fb8f856ed1
vasu1818 <40864108+vasu1818@users.noreply.github.com>
2018-07-05 23:10:51 +0530

    Update Vigenere cipher.java

--- Vigenere cipher.java
+++ Vigenere cipher.java
@@ -1,10 +1,14 @@
 class chp{
 	public static void main(String args[]){
-		String pt="this is not real",ct="";
+		String pt="this is not real",ct="",ot="";
 		int[] key={1,2,3,4} ;
 		for(int i=0; i<pt.length(); i++)
 				ct=ct+(char)(pt.charAt(i)+key[i%4]);
-		System.out.println(ct);
+		System.out.println("Encrypted Message : "+ct);
+
+		for(int i=0; i<ct.length(); i++)
+				ot=ot+(char)(ct.charAt(i)-key[i%4]);
+		System.out.println("Decrypted Message : "+ot);
 
 	}
 }
