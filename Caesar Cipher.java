b47f5f17caff0eb64247891b223a57f3e7eaa405
vasu1818 <40864108+vasu1818@users.noreply.github.com>
2018-07-15 19:46:31 +0530

    Update Caesar Cipher.java

--- Caesar Cipher.java
+++ Caesar Cipher.java
@@ -1,13 +1,13 @@
 class chp{
 	public static void main(String args[]){
 		String pt="HelloWorld",ct="",ot="";
-		int key=2 ;
-		for(int i=0; i<pt.length(); i++)
-				ct=ct+(char)(pt.charAt(i)+key);
-		System.out.println("Encrypted Message : "+ct);
+		int key=2 ; 
+		for(int i=0; i<pt.length(); i++) 			//For Encryption
+				ct=ct+(char)(pt.charAt(i)+key);		//Every character's ascii value of 
+		System.out.println("Encrypted Message : "+ct);		//plain text is increased by key
 
-		for(int i=0; i<ct.length(); i++)
-				ot=ot+(char)(ct.charAt(i)-key);
-		System.out.println("Decrypted Message : "+ot);
+		for(int i=0; i<ct.length(); i++)			//For Decryption
+				ot=ot+(char)(ct.charAt(i)-key);		//every character's ascii value of
+		System.out.println("Decrypted Message : "+ot);		//cipher text is increased by key
 	}
 }
