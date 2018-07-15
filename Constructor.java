a6c62cf13f5fa80ad1ae4282e57eee994d9e1e1c
vasu1818 <40864108+vasu1818@users.noreply.github.com>
2018-07-15 21:34:55 +0530

    Update Constructor.java

--- Constructor.java
+++ Constructor.java
@@ -1,18 +1,18 @@
 public class Constructor {
     {
-        System.out.println("Inside Instance block");
+        System.out.println("Inside Instance block");                //Instance block executes whenever the object is created
     }
     static{
-        System.out.println("Inside Static block");
+        System.out.println("Inside Static block");                  //static block executes first and only once
     }
     public Constructor() {
-        System.out.println("Constructor w/o Arguments");
+        System.out.println("Constructor w/o Arguments");            //constructor without arguments
     }
     public Constructor(int a) {
-        System.out.println("Constructor with 1 Arguments");
+        System.out.println("Constructor with 1 Arguments");         //constructor with 1 arguments
     }
     public Constructor(int a, int b) {
-        System.out.println("Constructor with 2 Arguments");
+        System.out.println("Constructor with 2 Arguments");         //constructor with 2 arguments
     }
     public static void main(String[] args) {
         Constructor cons1=new Constructor();
