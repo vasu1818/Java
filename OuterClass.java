14b2c68f494ed821265e936e819225862b19646e
vasu1818 <40864108+vasu1818@users.noreply.github.com>
2018-07-15 22:00:59 +0530

    Update OuterClass.java

--- OuterClass.java
+++ OuterClass.java
@@ -2,16 +2,16 @@ class OuterClass{
     public void fun(int a,int b, int c, int d){						//Function with 4 arguments
         System.out.println("Function 4 arguments\nSum is "+(a+b+c+d));
     }
-    public void fun(int a,int b, int c){							//Function with 3 arguments
+    public void fun(int a,int b, int c){						//Function with 3 arguments
         System.out.println("Function 3 arguments\nSum is "+(a+b+c));
     }
-    public void fun(int a,int b){									//Function with 2 arguments
+    public void fun(int a,int b){							//Function with 2 arguments
         System.out.println("Function with 2 arguments\nSum is "+(a+b));
     }
-    public void varArgs(int ...a){									//Accepts any no. of arguments
+    public void varArgs(int ...a){							//Accepts any no. of arguments
         int sum=0;
-		for(int i: a)												//i stores elements of a	
-            sum=sum+i;												//Adds all elements of a
+		for(int i: a)								//i stores elements of a	
+            sum=sum+i;									//Adds all elements of a
         System.out.println("Function with Variable Arguments\nSum is "+sum);
     }
 }
