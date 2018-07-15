cf0df6e4e3bc3c4607b4946498059d4ef45dadf3
vasu1818 <40864108+vasu1818@users.noreply.github.com>
2018-07-15 22:05:21 +0530

    Update Binary Search.java

--- Binary Search.java
+++ Binary Search.java
@@ -4,16 +4,16 @@ class BnSrch
 	int array[]={11,13,14,15,16,18,19,20,21,24};
     int find=18;//key to be found from the above array
 	int f=0;
-	int c=array.length; //length of array
+	int c=array.length; 				//length of array
 	if(find<array[c-1]){
     	while(f<=c){
-    	    int mid=(f+c)/2;// divides array into two parts
+    	    int mid=(f+c)/2;				// divides array into two parts
     		if(array[mid]>find)
     			c=mid-1;
     		else if(array[mid]==find){
     			System.out.println("Position of "+find+" is "+(mid+1));
-    			break;			// if the middle no. of array is what
-		}				// we want to find then it breaks
+    			break;				// if the middle no. of array is what
+		}					// we want to find then it breaks
     		else
     			f=mid+1;
 	    }
