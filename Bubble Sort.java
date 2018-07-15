5810be8fa0ed4566b79c866349d43e5276ecc9d5
vasu1818 <40864108+vasu1818@users.noreply.github.com>
2018-07-15 19:39:23 +0530

    Update Bubble Sort.java

--- Bubble Sort.java
+++ Bubble Sort.java
@@ -1,18 +1,18 @@
 class chp{
 	public static void main(String args[]){
-		int[] array={8,6,9,1,5,3,7,2,4};
+		int[] array={8,6,9,1,5,3,7,2,4}; 				//array to be sorted
 		for(int i=0; i<array.length; i++){
-	    	for(int j=0; j<array.length-1; j++){
-				if(array[j]>array[j+1]){
-				    int tmp=array[j];
-				    array[j]=array[j+1];
-				    array[j+1]=tmp;
-				}
-	    	}
+	    		for(int j=0; j<array.length-1; j++){
+					if(array[j]>array[j+1]){			//checks larger no. from two adjacent no.
+				    	int tmp=array[j];				//for
+				    	array[j]=array[j+1];			//swapping
+						array[j+1]=tmp;					//adjacent no.
+					}
+	    		}
 		}
 		System.out.print("Sorted Array :");
 		for(int i=0; i<array.length; i++)
-	    	System.out.print(" "+array[i]);
+	    	System.out.print(" "+array[i]);				//prints sorted array
 
 	}
 }
