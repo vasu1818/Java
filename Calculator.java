f456660b6fc7cd9fcf268b1262bb2696b0c6fa0b
vasu1818 <40864108+vasu1818@users.noreply.github.com>
2018-07-15 21:29:44 +0530

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -1,43 +1,44 @@
-import java.util.Scanner;
+import java.util.Scanner;                       //Scanner Class
 public class Calculator {
     public static void main(String[] args) {
         int a,b;
         Scanner s = new Scanner(System.in);
         do{
-        System.out.println("Enter 1st no.");
-        a=s.nextInt();
-        System.out.println("Enter 2nd no.");
-        b=s.nextInt();
-        if(a<0 || b<0)
-            System.out.println("\nEnter proper numbers!!");
-        }while(a<0 || b<0);
+            System.out.println("Enter 1st no.");
+            a=s.nextInt();
+            System.out.println("Enter 2nd no.");
+            b=s.nextInt();
+            if(a<0 || b<0){
+                System.out.println("\nEnter proper numbers!!");
+                continue;
+            }
+            System.out.println("Enter your operation to pe performed (+,-,*,/)");
+            char c=s.next().charAt(0);
+            if(c!='/' && c!='*' && c!='+' && c!='-')
+                System.out.println("\nEnter proper operator!!");
+            if(b==0 && c=='/')
+                System.out.println("Infinity");
+            }while(a<0 || b<0);                     //It wont accept any negative value and invalid operators
         
-        System.out.println("Enter your operation to pe performed (+,-,*,/)");
-        char c=s.next().charAt(0);
-        if(c!='/' && c!='*' && c!='+' && c!='-')
-            System.out.println("\nEnter proper operator!!");
-        if(b==0 && c=='/')
-            System.out.println("Infinity");
-        
-        switch(c){
+        switch(c){                                  //switch cases according to operators
             case '+':{
                 int ans=a+b;
-                System.out.println("sum of numbers is "+ans);
+                System.out.println("sum of numbers is "+ans);               //Addition
                 break;
             }
             case '-':{
                 int ans=a-b;
-                System.out.println("subtraction of numbers is "+ans);
+                System.out.println("subtraction of numbers is "+ans);       //Subtraction
                 break;
             }
             case '*':{
                 int ans=a*b;
-                System.out.println("Multiplication of numbers is "+ans);
+                System.out.println("Multiplication of numbers is "+ans);    //Multiplication
                 break;
             }
             case '/':{
                 int ans=a/b;
-                System.out.println("Divion of numbers is "+ans);
+                System.out.println("Divion of numbers is "+ans);            //Division
                 break;
             }
         }
