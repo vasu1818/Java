da0e035f20989288de7d01d43acb95ae49655dfd
vasu1818 <40864108+vasu1818@users.noreply.github.com>
2018-07-15 21:49:53 +0530

    Update GCD.java

--- GCD.java
+++ GCD.java
@@ -1,19 +1,18 @@
-import java.util.Scanner;
+import java.util.Scanner;						//Scanner class
 public class GCD {
-    public static void main(String[] args) {
-	Scanner s = new Scanner(System.in);
-        int[] a={10,20,30,50,40};
-	int c,d;
-        for(int i=0;i<a.length-1;i++){
-		while(a[i+1]>0){
-         	  c=a[i]/a[i+1];
-		  d=a[i]-c*a[i+1];
-         	  a[i]=a[i+1];
-	  	  a[i+1]=d;
-        	}
-	a[i+1]=a[i];
+    public static void main(String[] args){
+		Scanner s = new Scanner(System.in);
+		int[] a={10,20,30,50,40};
+		int c,d;
+		for(int i=0;i<a.length-1;i++){			//Finds GCD of first two numbers
+			while(a[i+1]>0){					//and then finds GCD of third number and the previous GCD
+				c=a[i]/a[i+1];					//and this continues till last number 
+				d=a[i]-c*a[i+1];
+				a[i]=a[i+1];
+				a[i+1]=d;
+			}
+			a[i+1]=a[i];
+		}
+			System.out.println(a[a.length-1]);	//Prints final GCD
 	}
-        System.out.println(a[a.length-1]);
-
-}
 }
