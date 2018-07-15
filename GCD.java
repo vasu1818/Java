516b15fd149d3e456cb9dcd859410e404e6e1b50
vasu1818 <40864108+vasu1818@users.noreply.github.com>
2018-07-15 22:01:35 +0530

    Update GCD.java

--- GCD.java
+++ GCD.java
@@ -1,12 +1,12 @@
-import java.util.Scanner;						//Scanner class
+import java.util.Scanner;					//Scanner class
 public class GCD {
     public static void main(String[] args){
 		Scanner s = new Scanner(System.in);
 		int[] a={10,20,30,50,40};
 		int c,d;
 		for(int i=0;i<a.length-1;i++){			//Finds GCD of first two numbers
-			while(a[i+1]>0){					//and then finds GCD of third number and the previous GCD
-				c=a[i]/a[i+1];					//and this continues till last number 
+			while(a[i+1]>0){			//and then finds GCD of third number and the previous GCD
+				c=a[i]/a[i+1];			//and this continues till last number 
 				d=a[i]-c*a[i+1];
 				a[i]=a[i+1];
 				a[i+1]=d;
