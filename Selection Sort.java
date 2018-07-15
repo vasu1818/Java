cd116b97b8518cc8fdd2f05e2b8432fc5571bd1c
vasu1818 <40864108+vasu1818@users.noreply.github.com>
2018-07-15 22:16:19 +0530

    Update Selection Sort.java

--- Selection Sort.java
+++ Selection Sort.java
@@ -1,20 +1,19 @@
-class SelSor
-{
-    public static void main(String args[])
-    {
-        int array[]={1,2,6,5,8,9,3,4};
+class SelSor{
+    public static void main(String args[]){
+        int array[]={1,2,6,5,8,9,3,4};          //array to be sorted
     	for(int i=0;i<array.length;i++){
-    		int chkpnt=i;
+    		int chkpnt=i;                       //It will store the index of the smallest element from array
     		for(int j=i+1;j<array.length;j++){
     			if(array[chkpnt]>array[j])
     				chkpnt=j;
     		}
-    		int tmp=array[i];
+    		int tmp=array[i];                   //Swaps the smallest number with the ith element
     		array[i]=array[chkpnt];
     		array[chkpnt]=tmp;
     	}
     	for(int i=0;i<array.length;i++){
-    		  System.out.println(array[i]);
+    		  System.out.println(array[i]);     //prints sorted array
+
     	}
     }
 }
